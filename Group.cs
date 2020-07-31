64d51fcb96fce34d554c61d7a5fb7996d9a288ad
Zεпяσх <66045929+Zenroxx@users.noreply.github.com>
2020-08-01 00:11:52 +0200

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Discord.REQ
+{
+    public class Group
+    {
+        public static string AddToGroup(string GroudId, string UserId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discordapp.com/api/v6/channels/{GroudId}/recipients/{UserId}");
+
+            var postData = "{}";
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "PUT";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public static string KickFromGroup(string GroudId, string UserId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discordapp.com/api/v6/channels/{GroudId}/recipients/{UserId}");
+
+            var postData = "{}";
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "DELETE";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public static string RenameGroup(string GroudId, string Name, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discordapp.com/api/v6/channels/{GroudId}");
+
+            var postData = "{" + $"\"name\": \"{Name}\"" + "}";
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+    }
+}
