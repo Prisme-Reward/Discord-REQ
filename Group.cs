6c594ae8a109a27088ef186c85c707b2af39c277
Zεпяσх <66045929+Zenroxx@users.noreply.github.com>
2020-08-01 00:17:05 +0200

    Update Group.cs

--- Group.cs
+++ Group.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -65,6 +65,31 @@ namespace Discord.REQ
             var data = Encoding.ASCII.GetBytes(postData);
 
             Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public static string LeaveGroup(string GroudId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://ptb.discordapp.com/api/v6/channels/{GroudId}");
+
+            var postData = "{}";
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "DELETE";
             Req.UserAgent = UserAgent;
             Req.ContentType = "application/json";
             Req.Headers.Add("authorization", Token);
