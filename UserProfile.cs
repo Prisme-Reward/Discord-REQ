2cd6e444e8e5411ca62de0d9760ecd84831285ef
Zεпяσх <66045929+Zenroxx@users.noreply.github.com>
2020-08-01 19:18:35 +0200

    Ajout de la méthode GetUser()
    Ajout de la méthode GetUser()

--- UserProfile.cs
+++ UserProfile.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -120,6 +120,20 @@ namespace Discord.REQ
             Req.ContentLength = Data.Length;
 
             using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string GetUser(string UserId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/users/{UserId}/profile");
+
+            Req.Method = "GET";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+
             var Response = (HttpWebResponse)Req.GetResponse();
             var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
             return ResponseInString;
