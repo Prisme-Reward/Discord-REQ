26cf68478e81634f01cedf57e73573b7fbd89071
Zεпяσх <66045929+Zenroxx@users.noreply.github.com>
2020-08-01 12:09:05 +0200

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -64,6 +64,176 @@ namespace Discord.REQ
             var Data = Encoding.ASCII.GetBytes("{}");
 
             Req.Method = "POST";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string BanUser(string UserId, string GuildId, string Token, string DeleteMessagesDays = "1", string Reason = "", string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/bans/{UserId}");
+            var Data = Encoding.ASCII.GetBytes("{" + $"\"delete_message_days\": \"{DeleteMessagesDays}\", \"reason\": \"{Reason}\"" + "}");
+
+            Req.Method = "PUT";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string UnBanUser(string UserId, string GuildId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/bans/{UserId}");
+            var Data = Encoding.ASCII.GetBytes("{}");
+
+            Req.Method = "DELETE";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string KickUser(string UserId, string GuildId, string Token, string Reason = "", string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/members/{UserId}?reason={Reason}");
+            var Data = Encoding.ASCII.GetBytes("{}");
+
+            Req.Method = "DELETE";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string CreateRole(string GuildId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles");
+            var Data = Encoding.ASCII.GetBytes("{}");
+
+            Req.Method = "POST";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string DeleteRole(string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{}");
+
+            Req.Method = "DELETE";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string SetRoleName(string Name, string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{" + $"\"name\": \"{Name}\"" + "}");
+
+            Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string SetRolePermissions(string PermissionNumber, string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{" + $"\"permissions\": \"{PermissionNumber}\"" + "}");
+
+            Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string SetRoleColor(string ColorNumber, string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{" + $"\"color\": \"{ColorNumber}\"" + "}"); // {"name":"rtrtrt","permissions":0,"color":0,"hoist":false,"mentionable":false}
+
+            Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string SetRoleMentionable(string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{\"mentionable\":\"true\"}");
+
+            Req.Method = "PATCH";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = Data.Length;
+
+            using (var stream = Req.GetRequestStream()) { stream.Write(Data, 0, Data.Length); }
+            var Response = (HttpWebResponse)Req.GetResponse();
+            var ResponseInString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+            return ResponseInString;
+        }
+
+        public static string SetRoleUnMentionable(string GuildId, string RoleId, string Token, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v6/guilds/{GuildId}/roles/{RoleId}");
+            var Data = Encoding.ASCII.GetBytes("{\"mentionable\":\"false\"}");
+
+            Req.Method = "PATCH";
             Req.UserAgent = UserAgent;
             Req.ContentType = "application/json";
             Req.Headers.Add("authorization", Token);
